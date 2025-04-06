ed82bc81ca04ad98ad4a9d3520059f976841f38e
ewald-n <77756984+ewald-n@users.noreply.github.com>
2025-04-06 15:19:17 -0400

    locustfile.py -- changed for the ratio of heavy user/ light user + created main.yml file

--- locustfile.py
+++ locustfile.py
@@ -30,3 +30,9 @@ class SimpleTasks(TaskSet):
 class SimpleUser(HttpUser):
     tasks = [SimpleTasks]
     wait_time = between(1, 5)  # Simulate a wait time between requests
+    weight = 1
+
+class HeavyUser(HttpUser):
+    tasks = [SimpleTasks]
+    wait_time = between(5, 10)  # plus lent
+    weight = 49
